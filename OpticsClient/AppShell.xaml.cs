c03f081a8ee7635fa819de1dafedfa575bbd8b33
Jiran Kurian <jiran.k@cloudbloom.io>
2022-10-14 15:57:37 +0530

    Initial Commit

---
+++ OpticsClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OpticsClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ OpticsClient/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OpticsClient;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
